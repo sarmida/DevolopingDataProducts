f86df7f6f1ceb47d706fa198c126c8b2e638eaee
Santiago Armida <sarmida@users.noreply.github.com>
2015-11-22 15:28:10 -0600

    Create iu.R
    iu with formating option for page with DIABETES information

---
+++ iu.R
@@ -0,0 +1,40 @@
+library(shiny)
+
+# Define UI for slider on diabetis
+shinyUI(fluidPage(
+  
+  #  Application title
+  titlePanel("Diabetes mg/dl input"),
+  
+  # Sidebar helps select value of Hg index
+  # options
+  sidebarLayout(
+    sidebarPanel(
+      # INDEX integer interval
+      sliderInput("glucosein", "mg/dl measure:",
+                  min=0, max=500, value=180),
+      submitButton('Submit')     
+    ),
+    
+    # Show a table with value selected
+    mainPanel(
+      #tableOutput("values"),
+      h3('Results of prediction'),
+      h4('You entered'), verbatimTextOutput("glucose")
+    )
+  )
+))
+#  shinyUI( pageWithSidebar(
+# Application title headerPanel("Diabetes prediction"),
+#sidebarPanel(
+#  numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5), 
+# submitButton('Submit')
+#), 
+# mainPanel(
+#) )
+#)
+#h3('Results of prediction'),
+#h4('You entered'), verbatimTextOutput("inputValue"), 
+#h4('Which resulted in a prediction of '), verbatimTextOutput("prediction")
+#) )
+#)
