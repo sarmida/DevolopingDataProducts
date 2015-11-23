1b5e8db597318a4764ef1d85ae279fe00377b827
Santiago Armida <sarmida@users.noreply.github.com>
2015-11-22 18:13:04 -0600

    Update server.R
    Update

--- server.R
+++ server.R
@@ -3,25 +3,24 @@ library(shiny)
 # Define server logic for slider of index selection
 shinyServer(function(input, output) {
   
-  # Reactive expression to compose a data frame containing 
-  # the values
+# Reactive expression to compose a data frame containing 
+# the values
   sliderValues <- reactive({
     
-    # Compose data frame
-    #data.frame(
-    #  Name = c("Index"),
-    #  Value = as.character(c(input$glucose)),   
-    #  stringsAsFactors=FALSE)
-    glucose <- input$glucosein
+# Compose data frame
+#data.frame(
+#  Name = c("Index"),
+#  Value = as.character(c(input$glucose)),   
+#  stringsAsFactors=FALSE)
+  glucose <- input$glucosein
   }) 
-  # CAlculae the risk
+# CAlculae the risk
   diabetesRisk <- function(glucose) glucose / 180
-  # Show the values using an HTML table
+# Show the values using an HTML table
     output$values <- renderPrint({input$glucosein
-    diabetesRisk <- function(glucose) glucose / 200
-    sliderValues()
-    #},
-    #output$inputValue <- renderPrint({input$glucose
+#sliderValues()
+#},
+#output$inputValue <- renderPrint({input$glucose
   })
 })
 #  diabetesRisk <- function(glucose) glucose / 200
